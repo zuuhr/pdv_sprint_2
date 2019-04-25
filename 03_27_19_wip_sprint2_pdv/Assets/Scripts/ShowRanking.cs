dac220b0b673e902a2d5eb24b5fe18383d525362
Darcor\Zuuhr <zuuhr.contact@gmail.com>
2019-04-25 10:48:59 +0200

    half half half half

---
+++ 03_27_19_wip_sprint2_pdv/Assets/Scripts/ShowRanking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowRanking : MonoBehaviour
+{
+    public Text ranking;//Object used for showing what we want in the canvas
+    string marcador;//Key used to access to the names saved in playerprefs
+    string marcador2;//Key used to access to the scores saved in playerprefs
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            marcador = "namep" + i;//to update the first key for accesing to another element of the playerprefs
+            marcador2 = "scoresp" + i;//to update the second key
+            ranking.text += "\n" + PlayerPrefs.GetString(marcador, "Desconocido");//we show in the canvas the name associated to the key
+            ranking.text += ": " + PlayerPrefs.GetInt(marcador2, 0);//as playerprefs can't work with different types simmultaneously, here we show the score associated to the key
+        }
+        
+    }
+}
---
+++ 03_27_19_wip_sprint2_pdv/Assets/Scripts/under13_variables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class under13_variables : MonoBehaviour
+{
+    public GameObject alien;
+    private bool alienAppearance;
+    private float timer;
+    //Every second an alien will appear
+    void Start() => timer = Time.time + 1; //set timer to spawn aliens
+
+    void Update()
+    {
+        if (!alienAppearance)
+        {
+            float timeLeft = timer - Time.time; //duration 
+            if (timeLeft < 0) alienAppearance = true;//time's up
+        }
+        else if (alienAppearance)
+        {
+            Debug.Log("Alien appearance"); //So we can check
+            Instantiate(alien, new Vector3(Random.Range(-12, 6), 0f, 16f), this.transform.rotation); 
+            alienAppearance = false;
+            timer = Time.time + 1; //set timer to spawn aliens
+        }
+    }
+}
